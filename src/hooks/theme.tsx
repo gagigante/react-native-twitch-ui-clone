2feae5e7e9f2278552670791d5341d8e7240b34c
Gabriel Gigante <gabriel_gigante@outlook.com>
2020-08-29 10:43:36 -0300

    Removing unused dependencies

--- src/hooks/theme.tsx
+++ src/hooks/theme.tsx
@@ -1,12 +1,9 @@
 import React, {
   createContext,
   useContext,
-  useState,
-  useEffect,
+  useState,  
   useCallback,
 } from 'react';
-
-import AsyncStorage from '@react-native-community/async-storage';
 
 import { ThemeProvider, DefaultTheme } from 'styled-components';
 
@@ -21,21 +18,7 @@ const ThemeContext = createContext<ThemeContextData>({} as ThemeContextData);
 
 const ColorThemeProvider: React.FC = ({ children }) => {
   const [selectedTheme, setSelectedTheme] = useState<DefaultTheme>(light);
-
-  // useEffect(() => {
-  //   async function loadTheme() {
-  //     const storedTheme = await AsyncStorage.getItem('@Twitch:theme');
-  
-  //     if (storedTheme) {
-  //       setSelectedTheme(JSON.parse(storedTheme));
-  //     }
-  
-  //     setSelectedTheme(light);
-  //   }
-
-  //   loadTheme();
-  // });
-
+ 
   const toggleTheme = useCallback(() => {
     setSelectedTheme(selectedTheme.title === 'light' ? dark : light);
   }, [selectedTheme]);
